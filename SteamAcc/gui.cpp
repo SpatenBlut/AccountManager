1e03faaadeb8bed82e6df4648d63ea8132ca0f0c
SpatenBlut <lxk4zzu@proton.me>
2026-03-04 01:47:22 +0100

    deleted comments

--- SteamAcc/gui.cpp
+++ SteamAcc/gui.cpp
@@ -1,15 +1,12 @@
 #include "gui.h"
 
-// Global DirectX 11 objects used throughout the program
 static ID3D11Device* g_device = nullptr;
 static ID3D11DeviceContext* g_ctx = nullptr;
 static IDXGISwapChain* g_swap = nullptr;
 static ID3D11RenderTargetView* g_rtv = nullptr;
 
-// Required so ImGui can forward Win32 input events to its handler
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND, UINT, WPARAM, LPARAM);
 
-// FIX: WndProc war undeclared - hier definiert
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wParam, lParam))
         return true;
@@ -20,7 +17,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
 
-// Gets the back buffer from the swap chain and creates a render target view from it
 void CreateRenderTarget() {
     ID3D11Texture2D* buf = nullptr;
     g_swap->GetBuffer(0, IID_PPV_ARGS(&buf));
@@ -29,38 +25,30 @@ void CreateRenderTarget() {
 }
 
 void setup() {
-    // FIX: hInst war undeclared - via GetModuleHandle holen
     HINSTANCE hInst = GetModuleHandle(nullptr);
 
-    // Get the main monitor resolution
     int screenW = GetSystemMetrics(SM_CXSCREEN);
     int screenH = GetSystemMetrics(SM_CYSCREEN);
 
-    // Register the window class and create a borderless fullscreen window
     WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0, 0, hInst, 0, 0, 0, 0, L"ImGui", 0 };
     RegisterClassExW(&wc);
     HWND hwnd = CreateWindowW(wc.lpszClassName, L"ImGui App",
         WS_POPUP, 0, 0, screenW, screenH, 0, 0, hInst, 0);
 
-    // Make the window transparent using a black color key
     SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED);
     SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
 
-    // Describe the swap chain, then create the DirectX 11 device and swap chain together
     DXGI_SWAP_CHAIN_DESC sd = {};
     sd.BufferCount = 2; sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
     sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
     sd.OutputWindow = hwnd; sd.SampleDesc.Count = 1;
     sd.Windowed = TRUE; sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
     D3D_FEATURE_LEVEL fl;
-    D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0,
-        nullptr, 0, D3D11_SDK_VERSION, &sd, &g_swap, &g_device, &fl, &g_ctx);
+    D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0, D3D11_SDK_VERSION, &sd, &g_swap, &g_device, &fl, &g_ctx);
 
-    // Create the render target and show the window
     CreateRenderTarget();
     ShowWindow(hwnd, SW_SHOWDEFAULT);
 
-    // Initialize ImGui and bind it to the Win32 window and DirectX 11 device
     ImGui::CreateContext();
     ImGui::StyleColorsDark();
     ImGui_ImplWin32_Init(hwnd);
@@ -81,11 +69,10 @@ void gui() {
             TranslateMessage(&msg); DispatchMessage(&msg); continue;
         }
 
-        // Tell ImGui a new frame is starting
         ImGui_ImplDX11_NewFrame();
         ImGui_ImplWin32_NewFrame();        
 
-        ImGui::NewFrame(); // start ui code
+        ImGui::NewFrame();
 
         ImGui::SetNextWindowSize(ImVec2(900, 600));
         ImGui::Begin("Account Manager", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
@@ -101,8 +88,6 @@ void gui() {
         }
         else
         {
-            
-
             static bool myCheckbox = false;
             static bool myButton = false;
 
@@ -122,9 +107,8 @@ void gui() {
             }
         }        
 
-		ImGui::End(); // end ui code
+		ImGui::End();
 
-        // Clear with pure black so the color key makes it fully transparent
         ImGui::Render();
         const float clear[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
         g_ctx->OMSetRenderTargets(1, &g_rtv, nullptr);
@@ -133,7 +117,6 @@ void gui() {
         g_swap->Present(1, 0);
     }
 
-    // Shutdown ImGui and release all DirectX 11 resources
     ImGui_ImplDX11_Shutdown();
     ImGui_ImplWin32_Shutdown();
     ImGui::DestroyContext();
